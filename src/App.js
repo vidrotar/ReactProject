1150867ddc8ea5db588e4c7344775c44d13a3590
Vid Rotar <vid.rotar1@gmail.com>
2019-04-13 15:53:42 +0200

    REACT adding state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: '3',
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: '2',
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class Developer {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+      name: 'local',
+    };
+  };
+
   render() {
+    const someone = new Developer('Jan', 'Hus');
+    console.log(someone.getName());
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Learning React</h1>
+        {this.state.list.map(item => {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{' ' + item.author}</span>
+              <span>{' ' + item.num_comments}</span>
+              <span>{' ' + item.points}</span>
+            </div>
+          )
+        })}
+        {'Name of state: ' + this.state.name}
       </div>
     );
   }
